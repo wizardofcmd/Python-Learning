fa9950dd27bc23a5fe36d2e5630beb0ab88246ca
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-09-28 22:50:54 +0100

    Finalizing Stack and Writing Test Cases

--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -1,19 +1,41 @@
-# Writing own Stack Data Structure Class
+# Writing my own Stack Data Structure Class
 # Will implement Test Cases
 
 class Stack():
+    """Class that allows for Stack functionality"""
     def __init__(self, collection, item):
         self.collection = []
         self.item = item
 
-    def push(self, collection, item):
-        collection.append(item)
+    def push(self, item):
+        """Push an item to the top of the stack."""
+        self.collection.append(item)
 
-    def peek(self, collection):
+    def peek(self):
+        """Look at the top of the stack."""
         # [-1] references the last element in the list
-        return collection[-1]
+        return self.collection[-1]
 
-    def pop(self, collection):
-        popped = collection[-1]
-        collection[-1].remove(popped)
+    def pop(self):
+        """Remove the last added item to the stack."""
+        popped = self.collection[-1]
+        self.collection[-1].remove(popped)
         return popped
+
+    def isEmpty(self):
+        """Check if the stack is empty."""
+        if self.collection:
+            return True
+        else:
+            return False
+
+    def clear(self):
+        """Clear all items in the stack."""
+        for item in self.collection:
+            self.collection.remove(item)
+
+    def showStack(self):
+        """Loop through the whole stack and print each item to the user."""
+        for item in self.collection:
+            print(item)
+        print()
---
+++ Data Structures/test_stack.py
@@ -0,0 +1,8 @@
+import unittest
+from stack import Stack
+
+class TestStack(unittest.TestCase):
+    """Testing the Stack class"""
+    def setUp(self):
+        self.collection = []
+        self.item = item
