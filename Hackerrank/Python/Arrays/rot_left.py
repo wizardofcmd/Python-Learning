5db1b9a442d986bdbd97eccd51de430810c8bfaf
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-10-11 14:47:47 +0100

    Hackerrank - Rotating Array Left

--- Hackerrank/Python/Arrays/rot_left.py
+++ Hackerrank/Python/Arrays/rot_left.py
@@ -1,4 +1,14 @@
 # a, being the list of integers
 # d, being the number of times to rotate left
+def rotLeft(a, d):
+    i = 0
+    while i < d:
+        for x in range(len(a)):
+            a[1] = a[x]
+        i += 1
+    return a
 
-def rotLeft(a, d)
+a = [1, 2, 3, 4, 5]
+d = 2
+result = rotLeft(a, d)
+print(result)
